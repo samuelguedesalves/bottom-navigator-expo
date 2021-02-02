8c55214baaa944390ea297c9a8844ad9a0750426
samuelguedesalves <samuelguedes7070@gmail.com>
2021-02-01 23:25:13 -0400

    initial navigation stack

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" options={{ headerShown: false }} component={Home} />
+        <Stack.Screen name="Profile" options={{ headerShown: false }} component={Profile} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Home ({ navigation }) {
+  return(
+    <>
+      <SafeAreaView/>
+      <View style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
+        <Text>Home</Text>
+
+        <TouchableOpacity 
+          style={{
+            width: '90%',
+            height: 50,
+            backgroundColor: '#ddd',
+            borderRadius: 8,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          onPress={ () => navigation.navigate('Profile') } 
+        >
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 24,
+            }}
+          >Move to Profile</Text>
+        </TouchableOpacity>
+
+      </View>
+    </>
+  )
+}
+function Profile ({ navigation }) {
+  return(
+    <>
+      <SafeAreaView/>
+      <View style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
+        <Text>Profile</Text>
+
+        <TouchableOpacity 
+          style={{
+            width: '90%',
+            height: 50,
+            backgroundColor: '#ddd',
+            borderRadius: 8,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          onPress={ () => navigation.navigate('Home') } 
+        >
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 24,
+            }}
+          >Move to Home</Text>
+        </TouchableOpacity>
+
+      </View>
+    </>
+  )
+}
